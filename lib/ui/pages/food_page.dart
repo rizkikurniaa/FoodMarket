7f86a7557a76b035df74ed069b0ce5fd9845dfde
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-01-30 01:07:39 +0700

    Done

--- lib/ui/pages/food_page.dart
+++ lib/ui/pages/food_page.dart
@@ -10,6 +10,9 @@ class _FoodPageState extends State<FoodPage> {
 
   @override
   Widget build(BuildContext context) {
+    double listItemWidth =
+        MediaQuery.of(context).size.width - 2 * defaultMargin;
+
     return ListView(
       children: [
         Column(
@@ -94,24 +97,28 @@ class _FoodPageState extends State<FoodPage> {
                     height: 16,
                   ),
                   Builder(builder: (_) {
-                    String body = (selectedIndex == 0)
-                        ? 'New Taste Body'
+                    List<Food> foods = (selectedIndex == 0)
+                        ? mockFoods
                         : (selectedIndex == 1)
-                            ? 'Popular Body'
-                            : 'Recommended Body';
-                    return Center(
-                      child: Text(
-                        body,
-                        style: blackFontStyle2,
-                      ),
+                            ? []
+                            : [];
+                    return Column(
+                      children: foods
+                          .map((e) => Padding(
+                                padding: EdgeInsets.fromLTRB(
+                                    defaultMargin, 0, defaultMargin, 16),
+                                child: FoodListitem(
+                                    food: e, itemWidth: listItemWidth),
+                              ))
+                          .toList(),
                     );
                   }),
-                  SizedBox(
-                    height: 80,
-                  )
                 ],
               ),
-            )
+            ),
+            SizedBox(
+              height: 80,
+            ),
           ],
         )
       ],
