eb6213e4d17375de6fc3bdf337129294b88b0103
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-01-29 11:14:35 +0700

    Created theme

---
+++ lib/shared/theme.dart
@@ -0,0 +1,7 @@
+part of 'shared.dart';
+
+const double defaultMargin = 24;
+Color mainColor = "FFC700".toColor();
+Color greyColor = "8D92A3".toColor();
+
+TextStyle greyFontStyle = GoogleFonts.poppins().copyWith(color: greyColor);
