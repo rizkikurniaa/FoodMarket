dc14fe371afea98f7d5e11de32bdd0289768b300
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-01-29 23:58:06 +0700

    Showing food card

--- lib/ui/pages/food_page.dart
+++ lib/ui/pages/food_page.dart
@@ -50,8 +50,9 @@ class _FoodPageState extends State<FoodPage> {
                   ),
                 ],
               ),
-            )
+            ),
             // LIST OF FOOD
+            FoodCard(mockFood)
             // LIST OF FOOD (TABS)
           ],
         )
