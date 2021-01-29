271fa7460412f6b5328c272e27a4fd5e9f765a53
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-01-29 22:09:52 +0700

    Call food page at home

--- lib/ui/pages/main_page.dart
+++ lib/ui/pages/main_page.dart
@@ -34,7 +34,7 @@ class _MainPageState extends State<MainPage> {
                 },
                 children: [
                   Center(
-                    child: Text('Home'),
+                    child: FoodPage(),
                   ),
                   Center(
                     child: Text('Order'),
