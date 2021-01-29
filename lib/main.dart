95dba39d130cfcea32c52dd0a5913b5c5f1ce25d
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-01-29 11:50:32 +0700

    Modified to sign in

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: GeneralPage(
-        onBackButtonPressed: () {},
-        child: Text('Body'),
-      ),
+      home: SignInPage(),
     );
   }
 }
