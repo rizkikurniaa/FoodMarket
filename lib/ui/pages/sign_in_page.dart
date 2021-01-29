0afce1dab97716bf4095176c3a61e083e4e0b1c2
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-01-29 20:38:20 +0700

    Updated to move another page

--- lib/ui/pages/sign_in_page.dart
+++ lib/ui/pages/sign_in_page.dart
@@ -100,7 +100,9 @@ class _SignInPageState extends State<SignInPage> {
                     color: mainColor,
                   )
                 : RaisedButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      Get.to(SignUpPage());
+                    },
                     elevation: 0,
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(8),
