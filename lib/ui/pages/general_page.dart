5b7dc1e31484ab32ee58e7d61420bdcfba6cc55b
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-01-29 11:14:52 +0700

    Modified general page ui

--- lib/ui/pages/general_page.dart
+++ lib/ui/pages/general_page.dart
@@ -1,10 +1,93 @@
 part of 'pages.dart';
 
 class GeneralPage extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final Function onBackButtonPressed;
+  final Widget child;
+  final Color backColor;
+
+  GeneralPage(
+      {this.title = 'Title',
+      this.subtitle = 'Subtitle',
+      this.onBackButtonPressed,
+      this.child,
+      this.backColor});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: Stack(
+        children: [
+          Container(
+            color: Colors.white,
+          ),
+          SafeArea(
+            child: Container(
+              color: backColor ?? "FAFAFC".toColor(),
+            ),
+          ),
+          SafeArea(
+            child: ListView(
+              children: [
+                Column(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(
+                        bottom: defaultMargin,
+                      ),
+                      padding: EdgeInsets.symmetric(
+                        horizontal: defaultMargin,
+                      ),
+                      width: double.infinity,
+                      height: 100,
+                      color: Colors.white,
+                      child: Row(
+                        children: [
+                          onBackButtonPressed != null
+                              ? Container(
+                                  width: 24,
+                                  height: 24,
+                                  margin: EdgeInsets.only(right: 26),
+                                  decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                      image:
+                                          AssetImage('assets/back_arrow.png'),
+                                    ),
+                                  ),
+                                )
+                              : SizedBox(),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                title,
+                                style: GoogleFonts.poppins(
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                              Text(
+                                subtitle,
+                                style: GoogleFonts.poppins(
+                                  color: "8D92A3".toColor(),
+                                  fontWeight: FontWeight.w300,
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                    child ?? SizedBox()
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
