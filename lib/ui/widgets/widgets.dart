2925faae9644e7a890220a9e14c08d608084e9e3
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-01-29 23:57:51 +0700

    Added some file

--- lib/ui/widgets/widgets.dart
+++ lib/ui/widgets/widgets.dart
@@ -1,2 +1,8 @@
+import 'package:FoodMarket/shared/shared.dart';
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:FoodMarket/models/models.dart';
+
 part 'custom_bottom_navbar.dart';
+part 'food_card.dart';
+part 'rating_stars.dart';
