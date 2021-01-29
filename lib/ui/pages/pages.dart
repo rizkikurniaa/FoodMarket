522611af55515ef0f12eb75f30f8b0ee0dfc9400
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-01-29 20:36:12 +0700

    Import get

--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:supercharged/supercharged.dart';
+import 'package:get/get.dart';
 
 part 'general_page.dart';
 part 'sign_in_page.dart';
+part 'sign_up_page.dart';
