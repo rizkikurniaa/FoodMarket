bf93aca1c6c27f0295e99a5a9671bc872f4ff2a3
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-01-29 22:09:06 +0700

    Added food page

---
+++ lib/ui/pages/food_page.dart
@@ -0,0 +1,61 @@
+part of 'pages.dart';
+
+class FoodPage extends StatefulWidget {
+  @override
+  _FoodPageState createState() => _FoodPageState();
+}
+
+class _FoodPageState extends State<FoodPage> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        Column(
+          children: [
+            // HEADER
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: defaultMargin),
+              color: Colors.white,
+              height: 100,
+              width: double.infinity,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Food Market',
+                        style: blackFontStyle1,
+                      ),
+                      Text(
+                        'Let`s get some food',
+                        style:
+                            greyFontStyle.copyWith(fontWeight: FontWeight.w300),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      image: DecorationImage(
+                        image: NetworkImage(
+                            'https://kepoper.com/wp-content/uploads/2019/12/dahyun-758x433.jpg'),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+            // LIST OF FOOD
+            // LIST OF FOOD (TABS)
+          ],
+        )
+      ],
+    );
+  }
+}
