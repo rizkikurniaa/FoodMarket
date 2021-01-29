61b4ba510c67f7d6e4c5c0b278c8f0248dd6b88f
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-01-29 20:37:47 +0700

    Modified to getmaterialapp

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import 'ui/pages/pages.dart';
 
@@ -9,7 +10,7 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       home: SignInPage(),
     );
