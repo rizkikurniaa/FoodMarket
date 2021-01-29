f47b7c3bbba61df74b66d7608fcafb165be2ccd5
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-01-30 01:07:21 +0700

    Added food list item

--- lib/ui/widgets/widgets.dart
+++ lib/ui/widgets/widgets.dart
@@ -1,5 +1,6 @@
 import 'package:FoodMarket/shared/shared.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:FoodMarket/models/models.dart';
 import 'package:supercharged/supercharged.dart';
@@ -8,3 +9,4 @@ part 'custom_bottom_navbar.dart';
 part 'food_card.dart';
 part 'rating_stars.dart';
 part 'custom_tabbar.dart';
+part 'food_list_item.dart';
