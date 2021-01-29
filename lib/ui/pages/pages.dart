781348fc72bbcd66f182af606c553f7646007c40
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-01-29 11:49:59 +0700

    Added sign in as part

--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -1,6 +1,8 @@
 import 'package:FoodMarket/shared/shared.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:supercharged/supercharged.dart';
 
 part 'general_page.dart';
+part 'sign_in_page.dart';
