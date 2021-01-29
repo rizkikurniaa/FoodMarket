f755331036071ac887d325a601f4b6c007ee5d9a
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-01-29 23:57:33 +0700

    Added models

--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -5,6 +5,7 @@ import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:supercharged/supercharged.dart';
 import 'package:get/get.dart';
+import 'package:FoodMarket/models/models.dart';
 
 part 'general_page.dart';
 part 'sign_in_page.dart';
