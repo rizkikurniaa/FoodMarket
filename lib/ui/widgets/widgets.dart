677a509a4281708a199c0cc15eafded3e4b3c665
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-01-30 00:40:55 +0700

    Added custom tabbar

--- lib/ui/widgets/widgets.dart
+++ lib/ui/widgets/widgets.dart
@@ -2,7 +2,9 @@ import 'package:FoodMarket/shared/shared.dart';
 import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:FoodMarket/models/models.dart';
+import 'package:supercharged/supercharged.dart';
 
 part 'custom_bottom_navbar.dart';
 part 'food_card.dart';
 part 'rating_stars.dart';
+part 'custom_tabbar.dart';
