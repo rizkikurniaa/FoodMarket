0031635eefa69a5256fe0e762cd9e3e61e273d13
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-01-29 11:15:20 +0700

    Imported some file

--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -1,3 +1,6 @@
+import 'package:FoodMarket/shared/shared.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:supercharged/supercharged.dart';
 
 part 'general_page.dart';
