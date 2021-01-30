a83734c25577c31fcfd6a6da0f88dde175273baa
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-01-30 23:42:59 +0700

    Created food detail page

---
+++ lib/ui/pages/food_detail_page.dart
@@ -0,0 +1,155 @@
+part of 'pages.dart';
+
+class FoodDetailPage extends StatefulWidget {
+  final Function onBackButtonPressed;
+  final Transaction transaction;
+
+  FoodDetailPage({this.onBackButtonPressed, this.transaction});
+
+  @override
+  _FoodDetailPageState createState() => _FoodDetailPageState();
+}
+
+class _FoodDetailPageState extends State<FoodDetailPage> {
+  int quantity = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            color: mainColor,
+          ),
+          SafeArea(
+            child: Container(
+              color: Colors.white,
+            ),
+          ),
+          SafeArea(
+            child: Container(
+              height: 300,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: NetworkImage(widget.transaction.food.picturePath),
+                    fit: BoxFit.cover),
+              ),
+            ),
+          ),
+          SafeArea(
+              child: ListView(
+            children: [
+              Column(
+                children: [
+                  //Back Button
+                  Container(
+                    height: 100,
+                    padding: EdgeInsets.symmetric(horizontal: defaultMargin),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: GestureDetector(
+                        onTap: () {
+                          if (widget.onBackButtonPressed != null) {
+                            widget.onBackButtonPressed();
+                          }
+                        },
+                        child: Container(
+                          padding: EdgeInsets.all(3),
+                          width: 30,
+                          height: 30,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8),
+                            color: Colors.black12,
+                          ),
+                          child: Image.asset('assets/back_arrow_white.png'),
+                        ),
+                      ),
+                    ),
+                  ),
+                  // Body
+                  Container(
+                    margin: EdgeInsets.only(top: 180),
+                    padding: EdgeInsets.symmetric(vertical: 26, horizontal: 16),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(20),
+                            topRight: Radius.circular(20)),
+                        color: Colors.white),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  widget.transaction.food.name,
+                                  style: blackFontStyle2,
+                                ),
+                                SizedBox(
+                                  height: 6,
+                                ),
+                                RatingStars(widget.transaction.food.rate),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                GestureDetector(
+                                  onTap: () {
+                                    setState(() {
+                                      quantity = max(1, quantity - 1);
+                                    });
+                                  },
+                                  child: Container(
+                                    width: 26,
+                                    height: 26,
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(8),
+                                        border: Border.all(width: 1),
+                                        image: DecorationImage(
+                                            image: AssetImage(
+                                                'assets/btn_min.png'))),
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: 50,
+                                  child: Text(
+                                    quantity.toString(),
+                                    textAlign: TextAlign.center,
+                                    style: blackFontStyle2,
+                                  ),
+                                ),
+                                GestureDetector(
+                                  onTap: () {
+                                    setState(() {
+                                      quantity = min(999, quantity + 1);
+                                    });
+                                  },
+                                  child: Container(
+                                    width: 26,
+                                    height: 26,
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(8),
+                                        border: Border.all(width: 1),
+                                        image: DecorationImage(
+                                            image: AssetImage(
+                                                'assets/btn_add.png'))),
+                                  ),
+                                )
+                              ],
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              )
+            ],
+          ))
+        ],
+      ),
+    );
+  }
+}
