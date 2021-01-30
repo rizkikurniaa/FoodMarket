59b2abd80636168578ca58a74e475d6c5630c09f
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-01-30 23:00:51 +0700

    Added override

--- lib/models/food.dart
+++ lib/models/food.dart
@@ -1,6 +1,6 @@
 part of 'models.dart';
 
-class Food {
+class Food extends Equatable {
   final int id;
   final String picturePath;
   final String name;
@@ -18,6 +18,10 @@ class Food {
     this.price,
     this.rate,
   });
+
+  @override
+  List<Object> get props =>
+      [id, picturePath, name, description, ingredients, price, rate];
 }
 
 List<Food> mockFoods = [
