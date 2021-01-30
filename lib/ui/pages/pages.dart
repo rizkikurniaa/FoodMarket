f71f583c8c3546a506de860c9d01313507ec2df3
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-01-31 00:06:24 +0700

    Import intl

--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -5,6 +5,7 @@ import 'package:FoodMarket/ui/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 import 'package:supercharged/supercharged.dart';
 import 'package:get/get.dart';
 import 'package:FoodMarket/models/models.dart';
