d9fbe61c87e842a3698b30776f353f34f098e6b5
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-01-31 23:53:02 +0700

    run main page

--- lib/main.dart
+++ lib/main.dart
@@ -11,16 +11,6 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: PaymentPage(
-        transaction: Transaction(
-          food: mockFoods[0],
-          user: mockUser,
-          quantity: 2,
-          total: (mockFoods[0].price * 2 * 1.1).toInt() + 50000,
-        ),
-      ),
-    );
+    return GetMaterialApp(debugShowCheckedModeBanner: false, home: MainPage());
   }
 }
