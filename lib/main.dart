80d0a89b12f3a6f654e42998b73f7a4b0e70bd37
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-02-01 20:02:44 +0700

    Run sign in

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:FoodMarket/cubit/cubit.dart';
+import 'package:FoodMarket/cubit/user_cubit.dart';
 import 'package:FoodMarket/models/models.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get.dart';
 
 import 'ui/pages/pages.dart';
@@ -11,9 +14,16 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MainPage(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => UserCubit()),
+        BlocProvider(create: (_) => FoodCubit()),
+        BlocProvider(create: (_) => TransactionCubit())
+      ],
+      child: GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: SignInPage(),
+      ),
     );
   }
 }
