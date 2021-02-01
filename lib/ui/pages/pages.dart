33769454030748128c6c758378a167718eff3ccb
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-02-01 20:29:12 +0700

    Import material icon

--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:supercharged/supercharged.dart';
 import 'package:get/get.dart';
 import 'package:FoodMarket/models/models.dart';
