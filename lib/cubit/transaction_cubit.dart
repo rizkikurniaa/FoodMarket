4c2c01cb6dcf44c1799eb4260602365f8bcf8063
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-02-01 19:41:10 +0700

    DONE transaction cubit

---
+++ lib/cubit/transaction_cubit.dart
@@ -0,0 +1,33 @@
+import 'package:FoodMarket/models/models.dart';
+import 'package:FoodMarket/services/services.dart';
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'transaction_state.dart';
+
+class TransactionCubit extends Cubit<TransactionState> {
+  TransactionCubit() : super(TransactionInitial());
+
+  Future<void> getTransactions() async {
+    ApiReturnValue<List<Transaction>> result =
+        await TransactionServices.getTransaction();
+
+    if (result.value != null) {
+      emit(TransactionLoaded(result.value));
+    } else {
+      emit(TransactionLoadingFailed(result.message));
+    }
+  }
+
+  Future<bool> submitTransaction(Transaction transaction) async {
+    ApiReturnValue<Transaction> result =
+        await TransactionServices.submitTransaction(transaction);
+    if (result.value != null) {
+      emit(TransactionLoaded(
+          (state as TransactionLoaded).transactions + [result.value]));
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
