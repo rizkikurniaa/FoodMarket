8baf6425789af3625b4e06edb0e000df91978e76
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-02-01 15:04:38 +0700

    Created user services

---
+++ lib/services/user_services.dart
@@ -0,0 +1,10 @@
+part of 'services.dart';
+
+class UserServices {
+  static Future<ApiReturnValue<User>> signIn(
+      String email, String password) async {
+    await Future.delayed(Duration(milliseconds: 500));
+
+    return ApiReturnValue(value: mockUser);
+  }
+}
