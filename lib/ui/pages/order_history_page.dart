b4f59039e54d30e59edf10034ed54138b002c948
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-02-02 14:42:52 +0700

    Food history integration

--- lib/ui/pages/order_history_page.dart
+++ lib/ui/pages/order_history_page.dart
@@ -7,100 +7,115 @@ class OrderHistoryPage extends StatefulWidget {
 
 class _OrderHistoryPageState extends State<OrderHistoryPage> {
   int selectedIndex = 0;
-  List<Transaction> inProgress = mockTransactions
-      .where((element) =>
-          element.status == TransactionStatus.on_delivery ||
-          element.status == TransactionStatus.pending)
-      .toList();
-
-  List<Transaction> past = mockTransactions
-      .where((element) =>
-          element.status == TransactionStatus.delivered ||
-          element.status == TransactionStatus.cancelled)
-      .toList();
 
   @override
   Widget build(BuildContext context) {
-    if (inProgress.length == 0 && past.length == 0) {
-      return IllustrationPage(
-        title: 'Ouch! Hungry',
-        subTitle: 'Seems you like have not\nordered any food yet',
-        picturePath: 'assets/love_burger.png',
-        buttonTap1: () {},
-        buttonTitle1: 'Find foods',
-      );
-    } else {
-      double listItemWidth =
-          MediaQuery.of(context).size.width - 2 * defaultMargin;
+    return BlocBuilder<TransactionCubit, TransactionState>(builder: (_, state) {
+      if (state is TransactionLoaded) {
+        if (state.transactions.length == 0) {
+          return IllustrationPage(
+            title: 'Ouch! Hungry',
+            subTitle: 'Seems you like have not\nordered any food yet',
+            picturePath: 'assets/love_burger.png',
+            buttonTap1: () {},
+            buttonTitle1: 'Find foods',
+          );
+        } else {
+          double listItemWidth =
+              MediaQuery.of(context).size.width - 2 * defaultMargin;
 
-      return ListView(
-        children: [
-          Column(
+          return ListView(
             children: [
-              //Header
-              Container(
-                height: 100,
-                width: double.infinity,
-                margin: EdgeInsets.only(bottom: defaultMargin),
-                padding: EdgeInsets.symmetric(horizontal: defaultMargin),
-                color: Colors.white,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Your Orders',
-                      style: blackFontStyle1,
+              Column(
+                children: [
+                  //Header
+                  Container(
+                    height: 100,
+                    width: double.infinity,
+                    margin: EdgeInsets.only(bottom: defaultMargin),
+                    padding: EdgeInsets.symmetric(horizontal: defaultMargin),
+                    color: Colors.white,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Your Orders',
+                          style: blackFontStyle1,
+                        ),
+                        Text(
+                          'Wait for the best meal',
+                          style: greyFontStyle.copyWith(
+                            fontWeight: FontWeight.w300,
+                          ),
+                        ),
+                      ],
                     ),
-                    Text(
-                      'Wait for the best meal',
-                      style: greyFontStyle.copyWith(
-                        fontWeight: FontWeight.w300,
-                      ),
+                  ),
+                  //Body
+                  Container(
+                    width: double.infinity,
+                    color: Colors.white,
+                    child: Column(
+                      children: [
+                        CustomTabBar(
+                          titles: ['In Progress', 'Past Orders'],
+                          selectedIndex: selectedIndex,
+                          onTap: (index) {
+                            setState(() {
+                              selectedIndex = index;
+                            });
+                          },
+                        ),
+                        SizedBox(
+                          height: 16,
+                        ),
+                        Builder(builder: (_) {
+                          List<Transaction> transactions = (selectedIndex == 0)
+                              ? state.transactions
+                                  .where((element) =>
+                                      element.status ==
+                                          TransactionStatus.on_delivery ||
+                                      element.status ==
+                                          TransactionStatus.pending)
+                                  .toList()
+                              : state.transactions
+                                  .where((element) =>
+                                      element.status ==
+                                          TransactionStatus.delivered ||
+                                      element.status ==
+                                          TransactionStatus.cancelled)
+                                  .toList();
+
+                          return Column(
+                            children: transactions
+                                .map((e) => Padding(
+                                      padding: EdgeInsets.only(
+                                        right: defaultMargin,
+                                        left: defaultMargin,
+                                        bottom: 16,
+                                      ),
+                                      child: OrderListItem(
+                                        transaction: e,
+                                        itemWidth: listItemWidth,
+                                      ),
+                                    ))
+                                .toList(),
+                          );
+                        })
+                      ],
                     ),
-                  ],
-                ),
+                  )
+                ],
               ),
-              //Body
-              Container(
-                width: double.infinity,
-                color: Colors.white,
-                child: Column(
-                  children: [
-                    CustomTabBar(
-                      titles: ['In Progress', 'Past Orders'],
-                      selectedIndex: selectedIndex,
-                      onTap: (index) {
-                        setState(() {
-                          selectedIndex = index;
-                        });
-                      },
-                    ),
-                    SizedBox(
-                      height: 16,
-                    ),
-                    Column(
-                      children: (selectedIndex == 0 ? inProgress : past)
-                          .map((e) => Padding(
-                                padding: EdgeInsets.only(
-                                  right: defaultMargin,
-                                  left: defaultMargin,
-                                  bottom: 16,
-                                ),
-                                child: OrderListItem(
-                                  transaction: e,
-                                  itemWidth: listItemWidth,
-                                ),
-                              ))
-                          .toList(),
-                    )
-                  ],
-                ),
-              )
             ],
-          ),
-        ],
-      );
-    }
+          );
+        }
+      } else {
+        return Center(
+          child: loadingIndicator,
+        );
+      }
+    });
   }
 }
