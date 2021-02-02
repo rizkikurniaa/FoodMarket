a69c0ced4cf2046deb21a286d80f5eda9c84e93a
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-02-02 14:21:49 +0700

    Food page integration

--- lib/ui/pages/food_page.dart
+++ lib/ui/pages/food_page.dart
@@ -62,38 +62,43 @@ class _FoodPageState extends State<FoodPage> {
             Container(
               height: 258,
               width: double.infinity,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: [
-                  Row(
-                    children: mockFoods
-                        .map((e) => Padding(
-                              padding: EdgeInsets.only(
-                                left:
-                                    (e == mockFoods.first) ? defaultMargin : 0,
-                                right: defaultMargin,
-                              ),
-                              child: GestureDetector(
-                                  onTap: () {
-                                    Get.to(
-                                      FoodDetailPage(
-                                        transaction: Transaction(
-                                            food: e,
-                                            user: (context
-                                                    .bloc<UserCubit>()
-                                                    .state as UserLoaded)
-                                                .user),
-                                        onBackButtonPressed: () {
-                                          Get.back();
-                                        },
+              child: BlocBuilder<FoodCubit, FoodState>(
+                builder: (_, state) => (state is FoodLoaded)
+                    ? ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: [
+                          Row(
+                            children: state.foods
+                                .map((e) => Padding(
+                                      padding: EdgeInsets.only(
+                                        left: (e == mockFoods.first)
+                                            ? defaultMargin
+                                            : 0,
+                                        right: defaultMargin,
                                       ),
-                                    );
-                                  },
-                                  child: FoodCard(e)),
-                            ))
-                        .toList(),
-                  )
-                ],
+                                      child: GestureDetector(
+                                          onTap: () {
+                                            Get.to(
+                                              FoodDetailPage(
+                                                transaction: Transaction(
+                                                    food: e,
+                                                    user: (context
+                                                            .bloc<UserCubit>()
+                                                            .state as UserLoaded)
+                                                        .user),
+                                                onBackButtonPressed: () {
+                                                  Get.back();
+                                                },
+                                              ),
+                                            );
+                                          },
+                                          child: FoodCard(e)),
+                                    ))
+                                .toList(),
+                          )
+                        ],
+                      )
+                    : Center(child: loadingIndicator),
               ),
             ),
             // LIST OF FOOD (TABS)
@@ -114,22 +119,31 @@ class _FoodPageState extends State<FoodPage> {
                   SizedBox(
                     height: 16,
                   ),
-                  Builder(builder: (_) {
-                    List<Food> foods = (selectedIndex == 0)
-                        ? mockFoods
-                        : (selectedIndex == 1)
-                            ? []
-                            : [];
-                    return Column(
-                      children: foods
-                          .map((e) => Padding(
-                                padding: EdgeInsets.fromLTRB(
-                                    defaultMargin, 0, defaultMargin, 16),
-                                child: FoodListitem(
-                                    food: e, itemWidth: listItemWidth),
-                              ))
-                          .toList(),
-                    );
+                  BlocBuilder<FoodCubit, FoodState>(builder: (_, state) {
+                    if (state is FoodLoaded) {
+                      List<Food> foods = state.foods
+                          .where((element) =>
+                              element.types.contains((selectedIndex == 0)
+                                  ? FoodType.new_food
+                                  : (selectedIndex == 1)
+                                      ? FoodType.popular
+                                      : FoodType.recommended))
+                          .toList();
+                      return Column(
+                        children: foods
+                            .map((e) => Padding(
+                                  padding: EdgeInsets.fromLTRB(
+                                      defaultMargin, 0, defaultMargin, 16),
+                                  child: FoodListitem(
+                                      food: e, itemWidth: listItemWidth),
+                                ))
+                            .toList(),
+                      );
+                    } else {
+                      return Center(
+                        child: loadingIndicator,
+                      );
+                    }
                   }),
                 ],
               ),
