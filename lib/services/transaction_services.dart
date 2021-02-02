089b3ada7b714ace7c348a7d4f228e7b68360d76
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-02-02 21:06:41 +0700

    Testing on error

--- lib/services/transaction_services.dart
+++ lib/services/transaction_services.dart
@@ -11,6 +11,7 @@ class TransactionServices {
       Transaction transaction) async {
     await Future.delayed(Duration(seconds: 2));
 
+    // return ApiReturnValue(message: "Transaksi gagal");
     return ApiReturnValue(
         value:
             transaction.copyWith(id: 123, status: TransactionStatus.pending));
