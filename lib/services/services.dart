b17bcfa1e3bb4496bd019b5771616e1765f9fcbc
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-02-17 22:51:00 +0700

    Import some lib and made baseUrl

--- lib/services/services.dart
+++ lib/services/services.dart
@@ -1,5 +1,11 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:FoodMarket/models/models.dart';
+import 'package:http/http.dart' as http;
 
 part 'user_services.dart';
 part 'food_services.dart';
 part 'transaction_services.dart';
+
+String baseUrl = 'http://foodmarket-backend.buildwithangga.id/api/';
