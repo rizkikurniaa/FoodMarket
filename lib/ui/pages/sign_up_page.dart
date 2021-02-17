113d82948ebba940fef19762fb25c8372ba6669f
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-02-18 00:33:53 +0700

    Integration signup with api

--- lib/ui/pages/sign_up_page.dart
+++ lib/ui/pages/sign_up_page.dart
@@ -6,12 +6,14 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpPageState extends State<SignUpPage> {
+  User user;
+  File pictureFile;
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController nameController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    TextEditingController emailController = TextEditingController();
-    TextEditingController passwordController = TextEditingController();
-    TextEditingController nameController = TextEditingController();
-
     return GeneralPage(
       title: 'Sign Up',
       subtitle: 'Register and eat',
@@ -20,25 +22,44 @@ class _SignUpPageState extends State<SignUpPage> {
       },
       child: Column(
         children: [
-          Container(
-            width: 110,
-            height: 110,
-            margin: EdgeInsets.only(top: 26),
-            padding: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/photo_border.png'),
-              ),
-            ),
+          GestureDetector(
+            onTap: () async {
+              PickedFile pickedFile =
+                  await ImagePicker().getImage(source: ImageSource.gallery);
+              if (pickedFile != null) {
+                pictureFile = File(pickedFile.path);
+                setState(() {});
+              }
+            },
             child: Container(
+              width: 110,
+              height: 110,
+              margin: EdgeInsets.only(top: 26),
+              padding: EdgeInsets.all(10),
               decoration: BoxDecoration(
-                shape: BoxShape.circle,
                 image: DecorationImage(
-                  image: NetworkImage(
-                      'https://kepoper.com/wp-content/uploads/2019/12/dahyun-758x433.jpg'),
-                  fit: BoxFit.cover,
+                  image: AssetImage('assets/photo_border.png'),
                 ),
               ),
+              child: (pictureFile != null)
+                  ? Container(
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                          image: FileImage(pictureFile),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    )
+                  : Container(
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                          image: AssetImage('assets/photo.png'),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
             ),
           ),
           Container(
@@ -106,6 +127,7 @@ class _SignUpPageState extends State<SignUpPage> {
               border: Border.all(color: Colors.black),
             ),
             child: TextField(
+              obscureText: true,
               controller: passwordController,
               decoration: InputDecoration(
                   border: InputBorder.none,
@@ -120,7 +142,13 @@ class _SignUpPageState extends State<SignUpPage> {
             padding: EdgeInsets.symmetric(horizontal: defaultMargin),
             child: RaisedButton(
               onPressed: () {
-                Get.to(AddressPage());
+                Get.to(AddressPage(
+                    User(
+                      name: nameController.text,
+                      email: emailController.text,
+                    ),
+                    passwordController.text,
+                    pictureFile));
               },
               elevation: 0,
               shape: RoundedRectangleBorder(
