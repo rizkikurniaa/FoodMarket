acc60296c829eb452d90c3346449c602a4d7afbf
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-02-18 23:04:24 +0700

    Added fromJson

--- lib/models/food.dart
+++ lib/models/food.dart
@@ -23,6 +23,26 @@ class Food extends Equatable {
     this.types = const [],
   });
 
+  factory Food.fromJson(Map<String, dynamic> data) => Food(
+      id: data['id'],
+      picturePath: data['picturePath'],
+      name: data['name'],
+      description: data['description'],
+      ingredients: data['ingredients'],
+      price: data['price'],
+      rate: (data['rate'] as num).toDouble(),
+      types: data['types'].toString().split(',').map((e) {
+        switch (e) {
+          case 'recommended':
+            return FoodType.recommended;
+            break;
+          case 'popular':
+            return FoodType.popular;
+          default:
+            return FoodType.new_food;
+        }
+      }).toList());
+
   @override
   List<Object> get props =>
       [id, picturePath, name, description, ingredients, price, rate];
