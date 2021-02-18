9e5ef1cb36484dec5900540cb7e57016c819f27e
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-02-18 08:08:05 +0700

    Integration login with API

--- lib/services/user_services.dart
+++ lib/services/user_services.dart
@@ -1,12 +1,29 @@
 part of 'services.dart';
 
 class UserServices {
-  static Future<ApiReturnValue<User>> signIn(
-      String email, String password) async {
-    await Future.delayed(Duration(milliseconds: 500));
+  static Future<ApiReturnValue<User>> signIn(String email, String password,
+      {http.Client client}) async {
+    if (client == null) {
+      client = http.Client();
+    }
 
-    return ApiReturnValue(value: mockUser);
-    // return ApiReturnValue(message: "Wrong email or password");
+    String url = baseUrl + 'login';
+
+    var response = await client.post(url,
+        headers: {"Content-Type": "application/json"},
+        body:
+            jsonEncode(<String, String>{'email': email, 'password': password}));
+
+    if (response.statusCode != 200) {
+      return ApiReturnValue(message: 'Please try again');
+    }
+
+    var data = jsonDecode(response.body);
+
+    User.token = data['data']['access_token'];
+    User value = User.fromJson(data['data']['user']);
+
+    return ApiReturnValue(value: value);
   }
 
   static Future<ApiReturnValue<User>> signUp(User user, String password,
@@ -44,7 +61,7 @@ class UserServices {
       if (result.value != null) {
         value = value.copyWith(
             picturePath:
-                'http://foodmarket-backend.buildwithangga.id/storage/' +
+                'https://foodmarket-backend.buildwithangga.id/storage/' +
                     result.value);
       }
     }
