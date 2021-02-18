42718ef7cc46bb8bbe34cf5297918d7f7474a9b5
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-02-18 08:07:31 +0700

    Changed url to Https

--- lib/services/services.dart
+++ lib/services/services.dart
@@ -8,4 +8,4 @@ part 'user_services.dart';
 part 'food_services.dart';
 part 'transaction_services.dart';
 
-String baseUrl = 'http://foodmarket-backend.buildwithangga.id/api/';
+String baseUrl = 'https://foodmarket-backend.buildwithangga.id/api/';
