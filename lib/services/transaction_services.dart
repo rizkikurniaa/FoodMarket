b6d06ce15d9c6bcc003033e16ba5e00f8ae5816e
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-02-23 20:39:25 +0700

    DONE Transaction services

--- lib/services/transaction_services.dart
+++ lib/services/transaction_services.dart
@@ -26,12 +26,33 @@ class TransactionServices {
   }
 
   static Future<ApiReturnValue<Transaction>> submitTransaction(
-      Transaction transaction) async {
-    await Future.delayed(Duration(seconds: 2));
+      Transaction transaction,
+      {http.Client client}) async {
+    client ??= http.Client();
 
-    // return ApiReturnValue(message: "Transaksi gagal");
-    return ApiReturnValue(
-        value:
-            transaction.copyWith(id: 123, status: TransactionStatus.pending));
+    String url = baseUrl + 'checkout';
+
+    var response = await client.post(url,
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Bearer ${User.token}"
+        },
+        body: jsonEncode(<String, dynamic>{
+          'food_id': transaction.food.id,
+          'user_id': transaction.user.id,
+          'quantity': transaction.quantity,
+          'total': transaction.total,
+          'status': 'PENDING'
+        }));
+
+    if (response.statusCode != 200) {
+      return ApiReturnValue(message: 'Please try again');
+    }
+
+    var data = jsonDecode(response.body);
+
+    Transaction value = Transaction.fromJson(data['data']);
+
+    return ApiReturnValue(value: value);
   }
 }
